1f147cf414ff029b87d62ce5cb7d1832d664b4e0
Mayko Rossi <168451655+MaykoRossi@users.noreply.github.com>
2024-08-11 10:10:26 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+# Atualizar o servidor
 echo "Atualizando o servidor..."
-apt-get update
-apt-get upgrade -y
-apt-get install apache2 -y
-apt-get install unzip -y
+sudo apt-get update && sudo apt-get upgrade -y
 
+# Instalar o Apache2
+echo "Instalando o Apache2..."
+sudo apt-get install apache2 -y
 
-echo "Baixando e copiando os arquivos da aplicação..."
+# Instalar o unzip
+echo "Instalando o unzip..."
+sudo apt-get install unzip -y
 
-cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
-cd linux-site-dio-main
-cp -R * /var/www/html/
+# Baixar a aplicação
+echo "Baixando a aplicação..."
+wget -O /tmp/linux-site-dio.zip https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
+# Descompactar a aplicação
+echo "Descompactando a aplicação..."
+sudo unzip /tmp/linux-site-dio.zip -d /tmp
 
+# Copiar os arquivos da aplicação para o diretório padrão do Apache
+echo "Copiando os arquivos da aplicação para o diretório do Apache..."
+sudo cp -r /tmp/linux-site-dio-main/* /var/www/html/
 
+# Limpar arquivos temporários
+echo "Limpando arquivos temporários..."
+rm /tmp/linux-site-dio.zip
+rm -rf /tmp/linux-site-dio-main
+
+# Reiniciar o Apache para garantir que as mudanças entrem em vigor
+echo "Reiniciando o Apache..."
+sudo systemctl restart apache2
+
+echo "Provisionamento concluído com sucesso!"
